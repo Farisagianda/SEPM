1b627c6dfdc4b8d06d6d260473059aa04f32b7ec
Abdurrahman Faris Agianda <s3640848@student.rmit.edu.au>
2018-05-04 12:08:14 +1000

    Changed errors

---
+++ jMoss/src/BookingTestCase.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookingTestCase {
+
+		@Test
+		void getName() {
+			Booking test = new Booking("Faris", "Faris.agianda@gmail.com", "Brunswick");
+			String output = test.getName();
+			assertEquals("Faris", output);
+		}
+		
+		@Test
+		void getEmail() {
+			Booking test = new Booking("Faris", "Faris.agianda@gmail.com", "Brunswick");
+			String output = test.getEmail();
+			assertEquals("Faris.agianda@gmail.com", output);
+		}
+		
+		@Test
+		void getSuburb() {
+			Booking test = new Booking("Faris", "Faris.agianda@gmail.com", "Brunswick");
+			String output = test.getSuburb();
+			assertEquals("Brunswick", output);
+		}
+
+}
--- jMoss/src/TheatreTestCase.java
+++ jMoss/src/TheatreTestCase.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class unitTest3 {
+class TheatreTestCase {
 
 	@Test
 	void test() {
