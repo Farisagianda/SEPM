ba578889159b83a5d935b0ea3fc6e5c24f37a4f9
Abdurrahman Faris Agianda <s3640848@student.rmit.edu.au>
2018-05-17 23:31:25 +1000

    Add a test file

---
+++ Sprint2/src/ShowingTestSprint2.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShowingTestSprint2 {
+
+	private static final Seatmap Seatmap = null;
+
+	@Test
+	void getTitle() {
+		Showing test = new Showing("StKilda", "The Avengers", "Monday", "19:00", Seatmap);
+		String output = test.getTitle();
+		assertEquals("The Avengers", output);
+	}
+	
+	@Test
+	void getTime() {
+		Showing test = new Showing("StKilda", "The Avengers", "Monday", "19:00", Seatmap);
+		String output = test.getTime();
+		assertEquals("19:00", output);
+	}
+	
+	@Test
+	void getDay() {
+		Showing test = new Showing("StKilda", "The Avengers", "Monday", "19:00", Seatmap);
+		String output = test.getDay();
+		assertEquals("Monday", output);
+	}
+	
+	@Test
+	void getLocation() {
+		Showing test = new Showing("StKilda", "The Avengers", "Monday", "19:00", Seatmap);
+		String output = test.getLocation();
+		assertEquals("StKilda", output);
+	}
+
+}
